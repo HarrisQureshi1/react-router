ca01c96b8311a37a399f011606e99aa2b0538477
harisqureshi <haris.qureshi@tplholdings.com>
2020-12-16 19:42:46 +0500

    Material UI Appbar and basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import logo from './logo.svg';
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    color: '#fff'
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
+  const shoes = {
+    "air-jordan-3-valor-blue": {
+      name: "VALOUR BLUE",
+      img:
+        "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+    },
+    "jordan-mars-270-london": {
+      name: "JORDAN MARS 270 LONDON",
+      img:
+        "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+    },
+    "air-jordan-1-zoom-racer-blue": {
+      name: "RACER BLUE",
+      img:
+        "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+    }
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit" component={Link} to={'/'}>Home</Button>
+          <Button color="inherit" component={Link} to={'Launch'}>Launch</Button>
+        </Toolbar>
+      </AppBar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Launch" element={<Launch />} />
+      </Routes>
+    </Router>
   );
+}
+
+function Home() {
+  return <div>
+    <h1>Welcome Home</h1>
+  </div>
+}
+
+function Launch() {
+  return <div>
+    <h1>Launch</h1>
+  </div>
 }
 
 export default App;
