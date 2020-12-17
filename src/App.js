ed551678134b9038542f63843699606d1e61f112
harisqureshi <catalyst1@gmail.com>
2020-12-17 17:40:49 +0500

    Nested routes weren't working, now working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom'
 
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -25,26 +25,28 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const shoes = {
+  "air-jordan-3-valor-blue": {
+    name: "VALOUR BLUE",
+    img:
+      "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  },
+  "jordan-mars-270-london": {
+    name: "JORDAN MARS 270 LONDON",
+    img:
+      "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  },
+  "air-jordan-1-zoom-racer-blue": {
+    name: "RACER BLUE",
+    img:
+      "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  }
+};
+
+
 function App() {
   const classes = useStyles();
 
-  const shoes = {
-    "air-jordan-3-valor-blue": {
-      name: "VALOUR BLUE",
-      img:
-        "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-    },
-    "jordan-mars-270-london": {
-      name: "JORDAN MARS 270 LONDON",
-      img:
-        "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-    },
-    "air-jordan-1-zoom-racer-blue": {
-      name: "RACER BLUE",
-      img:
-        "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-    }
-  };
   
   return (
     <Router>
@@ -62,7 +64,10 @@ function App() {
       </AppBar>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="Launch" element={<Launch />} />
+        <Route path="Launch" element={<Launch />} >
+          <Route path="/" element={<LaunchIndex />} />
+          <Route path=":slug" element={<LaunchShoe />} />
+        </Route>
       </Routes>
     </Router>
   );
@@ -75,9 +80,38 @@ function Home() {
 }
 
 function Launch() {
-  return <div>
+  return (<div>
     <h1>Launch</h1>
-  </div>
+    <Outlet />
+  </div>)
+}
+
+function LaunchIndex() {
+  return (<div>
+    <h1>Hello</h1>
+    <ul>
+      {Object.entries(shoes).map(([slug, {name, img}]) => 
+      <li key={slug}>
+        <h2>{name}</h2>
+        <img src={img} alt=""/>
+      </li>
+      )}
+    </ul>
+  </div>)
+}
+
+function LaunchShoe() {
+  return (<div>
+    <h1>Hello</h1>
+    <ul>
+      {Object.entries(shoes).map(([slug, {name, img}]) => 
+      <li key={slug}>
+        <h2>{name}</h2>
+        <img src={img} alt=""/>
+      </li>
+      )}
+    </ul>
+  </div>)
 }
 
 export default App;
